3cea5daaa135da9cf081c042d802318bdf2e9927
makotow <makoto.wata@gmail.com>
2012-08-14 23:26:28 +0900

    Add utility enviroment variables.

--- custom/custom-env.zsh
+++ custom/custom-env.zsh
@@ -1,19 +1,20 @@
 ##
 
-export EDITOR=vim
+export EDITOR=emacsclient
 export PAGER=less
-#export PAGER='--tabs=4 --no-init --LONG-PROMPT --ignore-case'
-
 export LESS='-R'
 export LESSOPEN='| /usr/local/bin/src-hilite-lesspipe.sh %s'
-
+export LV='-Ou8 -c'
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/1.7.0.jdk/Contents/Home/
+
+# 3秒以上かかった処理は詳細表示
+REPORTTIME=3
+
 #=============================
 # source z.sh
 #=============================
 if [ -f /usr/local/Cellar/z/1.1/etc/profile.d/z.sh ]; then
-_Z_CMD=j
-#source ~/z/z.sh
+    _Z_CMD=j
    source /usr/local/Cellar/z/1.1/etc/profile.d/z.sh
    precmd() {
      _z --add "$(pwd -P)"

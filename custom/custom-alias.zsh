88ea7abb6bbd2f67b375c842bbb4178d397cf8da
makotow <makoto.wata@gmail.com>
2012-09-22 04:38:37 +0900

    Clean up

--- custom/custom-alias.zsh
+++ custom/custom-alias.zsh
@@ -4,11 +4,6 @@
 # Alias configuration
 #  expand aliases before completing
 ###############################
-
-# alias ls='ls -F'
-# alias dir='ls --format=vertical' dir='ls -lF'
-# alias gd='dirs -v; echo -n "select number: " ; read newdir; cd -"$newdir"'
-# alias l1='ls -1' l='ls -lFAo' la='ls -a' lf='ls -AF' ll='ls -l' lr='ls -RF' lsa='ls -a' lsal='ls -al' lsl='ls -l' lt='ls -lFAot' vdir='ls --color=auto --format=long'
 alias ls='ls -GF'
 alias lsal='ls -al'
 
@@ -17,7 +12,7 @@ alias lsal='ls -al'
 ###############################
 alias mvn='/usr/local/bin/mvn'
 alias emacs='/usr/local/Cellar/emacs/24.2/Emacs.app/Contents/MacOS/Emacs'
-#alias play='~/bin/libexec/play/play'
+
 ###############################
 # cover mistype
 ###############################
@@ -43,5 +38,3 @@ alias -g H='|head'
 alias -g T='|tail'
 alias -g G='|grep'
 alias -g V='|vim -R -'
-#alias -g L='|--help|head'
-#alias -g L='|--help|less'
